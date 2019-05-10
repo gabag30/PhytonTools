86aa51ba2ae5ea4aa2c0f66bbfe732f95f1d49f5
GB <40696325+gabag30@users.noreply.github.com>
2019-05-10 16:17:09 +0200

    update acuerdo a talend

--- moveNiDbfToPostgre2.py
+++ moveNiDbfToPostgre2.py
@@ -96,49 +96,49 @@ def silentremove(filename):
         if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
             raise # re-raise exception if a different error occurred
 #--------------------------------------------------------------------------------------------------------
-con = psycopg2.connect(dbname='postgres',
-      user=args.adminUser, host=args.hostIp,
-      password=args.adminPass)
-
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
-
-cur = con.cursor()
-try:
-    cur.execute("DROP DATABASE IF EXISTS %s  ;" % args.interMedDb)
-except psycopg2.Error:
-    raise SystemExit(
-        'Failed to setup Postgres environment.\n{0}'.format(sys.exc_info())
-    )
-try:
-    cur.execute("CREATE DATABASE %s  ;" % args.interMedDb)
-except psycopg2.Error:
-    raise SystemExit(
-        'Failed to setup Postgres environment.\n{0}'.format(sys.exc_info())
-    )
-cur.close()
-con.close()
-con2 = psycopg2.connect(dbname=args.interMedDb,
-      user=args.adminUser, host=args.hostIp,
-      password=args.adminPass)
-
-con2.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
-
-cur2 = con2.cursor()
-try:
-    cur2.execute("CREATE SCHEMA %s  ;" % args.schemaDb)
-except psycopg2.Error:
-    raise SystemExit(
-        'Failed to crete schema.\n{0}'.format(sys.exc_info())
-    )
-try:
-    cur2.execute("COMMIT")
-except psycopg2.Error:
-    raise SystemExit(
-        'Failed to commit.\n{0}'.format(sys.exc_info())
-    )
-
-cur2.close()
-con2.close()
+# con = psycopg2.connect(dbname='postgres',
+#       user=args.adminUser, host=args.hostIp,
+#       password=args.adminPass)
+#
+# con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
+#
+# cur = con.cursor()
+# try:
+#     cur.execute("DROP DATABASE IF EXISTS %s  ;" % args.interMedDb)
+# except psycopg2.Error:
+#     raise SystemExit(
+#         'Failed to setup Postgres environment.\n{0}'.format(sys.exc_info())
+#     )
+# try:
+#     cur.execute("CREATE DATABASE %s  ;" % args.interMedDb)
+# except psycopg2.Error:
+#     raise SystemExit(
+#         'Failed to setup Postgres environment.\n{0}'.format(sys.exc_info())
+#     )
+# cur.close()
+# con.close()
+# con2 = psycopg2.connect(dbname=args.interMedDb,
+#       user=args.adminUser, host=args.hostIp,
+#       password=args.adminPass)
+#
+# con2.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) # <-- ADD THIS LINE
+#
+# cur2 = con2.cursor()
+# try:
+#     cur2.execute("CREATE SCHEMA %s  ;" % args.schemaDb)
+# except psycopg2.Error:
+#     raise SystemExit(
+#         'Failed to crete schema.\n{0}'.format(sys.exc_info())
+#     )
+# try:
+#     cur2.execute("COMMIT")
+# except psycopg2.Error:
+#     raise SystemExit(
+#         'Failed to commit.\n{0}'.format(sys.exc_info())
+#     )
+#
+# cur2.close()
+# con2.close()
 
 conex = connectSchemaPostgre( args.hostIp, args.interMedDb, args.adminUser,args.adminPass,args.schemaDb)
 dirpath = os.getcwd()
